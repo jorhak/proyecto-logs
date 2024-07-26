d803142a1f3d7515ec6f3ca93092094290941ce0
Pierluigi Vaca Garcia <0077amaterasu16@gmail.com>
2024-07-26 04:06:13 +0000

    Script finalizados

--- archivar.sh
+++ archivar.sh
@@ -2,8 +2,8 @@
 # Filename: archivar
 # Description: Crea backups de los logs
 
-RUTA=/home/pablo/Documentos/Logs/
-DESTINO=/home/pablo/Documentos/serviciosLog/
+RUTA=/home/jon/logs/
+DESTINO=/home/jon/backup/
 declare -a FILES=(Apache Windows Hadoop Android Zookeeper)
 
 if [ $# -gt 2 ]
@@ -15,8 +15,12 @@ fi
 
 for file in "${FILES[@]}"
 do
-  cd "$RUTA$file"
-  tar -zcvf $file$Fecha".tar.gz" $file"_2k.log"
-  mv $file$Fecha".tar.gz" $DESTINO$file
+  if [ ! -d "$DESTINO$file" ]
+  then
+     mkdir -p "$DESTINO$file"
+  fi
+  cd "$RUTA$file" || { echo "No se pudo cambiar al directorio $RUTA$file"; exit 1; }
+  tar -zcvf "${file}${Fecha}.tar.gz" "${file}_2k.log" 2>&1
+  mv "${file}${Fecha}.tar.gz" "${DESTINO}${file}" || { echo "No se pudo mover el archivo ${file}${Fecha}.tar.gz a $DESTINO$file"; exit 1; }
 done
 
--- rotacion.sh
+++ rotacion.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
-# Filename: archivar
-# Description: Crea backups de los logs
+# Filename: rotacion
+# Description: Mueve los ficheros .tar.gz al servidor
+# remoto
 
+
+BACKUP=/home/jon/backup/
+declare -a SERVICES=(Apache Windows Hadoop Android Zookeeper)
 if [ $# -gt 1 ]
 then
-  Mes=$2
-  Anio=$1
+  MONTH=$2
+  YEAR=$1
 else
-  Mes=$(date +%m)
-  Anio=$(date +%Y)
+  MONTH=$(date +%m)
+  YEAR=$(date +%Y)
 fi
-
-mkdir -p "./$Anio/$Mes"
-mv ./serviciosLog/* "./$Anio/$Mes"
-
-#echo "$Mes  $Anio"
-scp -r "./$Anio" pablo@192.168.0.22:/home/pablo/Descargas
-cd ./serviciosLog
-mkdir Apache Android Windows Hadoop Zookeeper
+for SERVICE in "${SERVICES[@]}"
+do
+   if [ -d "$BACKUP$SERVICE" ]
+   then
+      ssh jon@remoto "mkdir -p ${BACKUP}${SERVICE}/${YEAR}/${MONTH}"
+      FILES_TO_COPY="${BACKUP}${SERVICE}/${SERVICE}${YEAR}${MONTH}*.tar.gz"
+      for file in $FILES_TO_COPY
+      do
+        if [ -e "$file" ]; then
+          scp "$file" jon@remoto:"${BACKUP}${SERVICE}/${YEAR}/${MONTH}"
+        else
+          echo "No hay archivos para copiar para el servicio $SERVICE en ${YEAR}-${MONTH}"
+        fi
+      done
+      DATE_OLD=$(date -d "${YEAR}-${MONTH}-01 -3 month" +%Y%m)
+      YEAR_OLD=${DATE_OLD:0:4}
+      MONTH_OLD=${DATE_OLD:4:2}
+    
+      FILES_TO_DELETE="${BACKUP}${SERVICE}/${SERVICE}${YEAR_OLD}${MONTH_OLD}*.tar.gz"
+      if ls $FILES_TO_DELETE 1> /dev/null 2>&1 
+      then
+         rm $FILES_TO_DELETE
+      else
+         echo "No hay archivos para eliminar para el servicio $SERVICE en ${YEAR_OLD}-${MONTH_OLD}"
+      fi   
+   else
+      echo "El directorio <<${BACKUP}${SERVICE}>> no existe"   
+   fi
+done
